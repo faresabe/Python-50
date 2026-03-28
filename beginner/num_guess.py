61d8a08664c258e1de3ad65403449ebce60b3469
faresabe <faresabenet9@gmail.com>
2026-03-28 11:45:12 +0300

    added the num_guess code

--- beginner/num_guess.py
+++ beginner/num_guess.py
@@ -1,1 +1,38 @@
-rr
+import random
+import math
+UpperBound = int(input("Enter the Upper Bound Number: "))
+LowerBound = int(input("Enter the Lower Bound Number: "))
+
+
+if LowerBound > UpperBound:
+    print("Lower bound must be less than upper bound")
+    exit()
+
+     
+
+Num = random.randint(LowerBound,UpperBound)
+Loop = int(math.ceil(math.log(UpperBound - LowerBound + 1 ,2)))
+Won = False
+
+while Loop > 0:
+    
+    try:
+         Ask = int(input("Guess A Number: "))
+    except ValueError:
+        print("Enter a valid number")
+    
+
+
+   
+    if Ask > Num:
+        print("Number too High Try Again")
+    elif Ask < Num:
+        print("Number too Low Try Again")
+    else:
+        Won = True
+        print("Congra , YOU WON")
+        break
+    Loop -=1  
+
+if Won == False:
+  print("You Lose the number was," , Num)  

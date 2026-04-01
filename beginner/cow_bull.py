7cf5b6f139419a67c055372143861f75ad96258b
faresabe <faresabenet9@gmail.com>
2026-04-01 22:41:05 +0300

    modified

--- beginner/cow_bull.py
+++ beginner/cow_bull.py
@@ -1,15 +1,10 @@
-# Import required module
+
 import random 
 
-# Returns list of digits 
-# of a number
 def getDigits(num):
     return [int(i) for i in str(num)]
     
-
-# Returns True if number has 
-# no duplicate digits 
-# otherwise False      
+      
 def noDuplicates(num):
     num_li = getDigits(num)
     if len(num_li) == len(set(num_li)):
@@ -17,9 +12,7 @@ def noDuplicates(num):
     else:
         return False
 
-
-# Generates a 4 digit number 
-# with no repeated digits    
+    
 def generateNum():
     while True:
         num = random.randint(1000,9999)
@@ -27,9 +20,6 @@ def generateNum():
             return num
 
 
-# Returns common digits with exact 
-# matches (bulls) and the common 
-# digits in wrong position (cows)
 def numOfBullsCows(num,guess):
     bull_cow = [0,0]
     num_li = getDigits(num)
@@ -37,26 +27,25 @@ def numOfBullsCows(num,guess):
     
     for i,j in zip(num_li,guess_li):
         
-        # common digit present
+        
         if j in num_li:
         
-            # common digit exact match
+            
             if j == i:
                 bull_cow[0] += 1
             
-            # common digit match but in wrong position
+            
             else:
                 bull_cow[1] += 1
                 
     return bull_cow
     
     
-# Secret Code
+
 num = generateNum()
 tries =int(input('Enter number of tries: '))
 
-# Play game until correct guess 
-# or till no tries left
+
 while tries > 0:
     guess = int(input("Enter your guess: "))
     

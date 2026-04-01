109cfb9863b2093f51b4baa5f008c3b1991cf89f
faresabe <faresabenet9@gmail.com>
2026-04-01 22:46:33 +0300

    modifiy rock paper

--- beginner/rock_paper.py
+++ beginner/rock_paper.py
@@ -1,6 +1,6 @@
 import random
 
-# Print multiline instruction
+
 print('Winning rules of the game ROCK PAPER SCISSORS are:\n'
       + "Rock vs Paper -> Paper wins \n"
       + "Rock vs Scissors -> Rock wins \n"

5d5cc8b363e20f4093353786e5484aa26e97743f
faresabe <faresabenet9@gmail.com>
2026-04-01 22:45:57 +0300

    modifiy rock paper

--- beginner/rock_paper.py
+++ beginner/rock_paper.py
@@ -10,14 +10,14 @@ while True:
 
     print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
 
-    # Take the input from user
+    
     choice = int(input("Enter your choice: "))
 
-    # Looping until user enters valid input
+    
     while choice > 3 or choice < 1:
         choice = int(input('Enter a valid choice please ☺: '))
 
-    # Initialize value of choice_name variable corresponding to the choice value
+    
     if choice == 1:
         choice_name = 'Rock'
     elif choice == 2:
@@ -25,14 +25,14 @@ while True:
     else:
         choice_name = 'Scissors'
 
-    # Print user choice
+    
     print('User choice is:', choice_name)
     print("Now it's Computer's Turn...")
 
-    # Computer chooses randomly any number among 1, 2, and 3
+    
     comp_choice = random.randint(1, 3)
 
-    # Initialize value of comp_choice_name variable corresponding to the choice value
+    
     if comp_choice == 1:
         comp_choice_name = 'Rock'
     elif comp_choice == 2:
@@ -43,7 +43,7 @@ while True:
     print("Computer choice is:", comp_choice_name)
     print(choice_name, 'vs', comp_choice_name)
 
-    # Determine the winner
+    
     if choice == comp_choice:
         result = "DRAW"
     elif (choice == 1 and comp_choice == 2) or (comp_choice == 1 and choice == 2):
@@ -53,7 +53,7 @@ while True:
     elif (choice == 2 and comp_choice == 3) or (comp_choice == 2 and choice == 3):
         result = 'Scissors'
 
-    # Print the result
+   
     if result == "DRAW":
         print("<== It's a tie! ==>")
     elif result == choice_name:
@@ -61,11 +61,11 @@ while True:
     else:
         print("<== Computer wins! ==>")
 
-    # Ask if the user wants to play again
+ 
     print("Do you want to play again? (Y/N)")
     ans = input().lower()
     if ans == 'n':
         break
 
-# After coming out of the while loop, print thanks for playing
+
 print("Thanks for playing!")

fb74ac76dfa5a071eb24bf4c22aecda202e90484
faresabe <faresabenet9@gmail.com>
2026-04-03 22:25:15 +0300

    modified mastermind python file

--- beginner/mastermind.py
+++ beginner/mastermind.py
@@ -1,46 +1,39 @@
 import random
 
-# the .randrange() function generates a
-# random number within the specified range.
+
 num = random.randrange(1000, 10000)
-num_str = str(num)  # Convert once
+num_str = str(num)  
 
 n = int(input("Guess the 4 digit number:"))
 
-# condition to test equality of the
-# guess made. Program terminates if true.
+
 if (n == num):
     print("Great! You guessed the number in just 1 try! You're a Mastermind!")
 else:
-    # ctr variable initialized. It will keep count of
-    # the number of tries the Player takes to guess the number.
+    
     ctr = 0
 
-    # while loop repeats as long as the
-    # Player fails to guess the number correctly.
+    
     while (n != num):
-        # variable increments every time the loop
-        # is executed, giving an idea of how many
-        # guesses were made.
+       
         ctr += 1
 
-        # explicit type conversion of an integer to
-        # a string in order to ease extraction of digits
+        
         n_str = str(n)
 
         bulls = 0
         cows = 0
         used = [False] * 4
 
-        # First, count bulls (correct position)
+        
         for i in range(4):
             if n_str[i] == num_str[i]:
                 bulls += 1
                 used[i] = True
 
-        # Then, count cows (correct digit, wrong position)
+        
         for i in range(4):
-            if not used[i]:  # Only check if not already a bull
+            if not used[i]: 
                 for j in range(4):
                     if not used[j] and n_str[i] == num_str[j]:
                         cows += 1
@@ -48,11 +41,11 @@ else:
                         break
 
         if bulls == 4:
-            break  # Should not happen here, but safety
+            break 
 
         print(f"Not quite the number. Bulls: {bulls}, Cows: {cows}")
         n = int(input("Enter your next choice of numbers: "))
 
-    # After loop, success
+   
     print("You've become a Mastermind!")
     print("It took you only", ctr + 1, "tries.")

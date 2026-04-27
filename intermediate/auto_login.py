be87b0f549b1b03461e04f87845d0ddf3017d509
faresabe <faresabenet9@gmail.com>
2026-04-27 19:38:03 +0300

    fix code

--- intermediate/auto_login.py
+++ intermediate/auto_login.py
@@ -27,14 +27,10 @@ def startBot(username, password, url):
         "id/class/name/css selector of login button").click()
 
 
-# Driver Code
-# Enter below your login credentials
 username = "Enter your username"
 password = "Enter your password"
 
-# URL of the login page of site
-# which you want to automate login.
 url = "Enter the URL of login page of website"
 
-# Call the function
+
 startBot(username, password, url)

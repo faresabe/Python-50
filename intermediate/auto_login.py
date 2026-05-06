66fcf3162b0f015bafda79b2060448f27fa43adc
faresabe <faresabenet9@gmail.com>
2026-05-06 08:07:53 +0300

    m

--- intermediate/auto_login.py
+++ intermediate/auto_login.py
@@ -12,7 +12,7 @@ def startBot(username, password, url):
     
     # opening the website  in chrome.
     driver.get(url)  
-      
+       
     # find the id or name or class of
     # username by inspecting on username input
     driver.find_element_by_name(
